3459639ad8be8c2bd049d2f963227cacc581de0e
alina.gryaznova <alina.gryaznova@sigmaukraine.com>
2016-02-24 16:52:25 +0200

    some style changes

--- TestLibrary/TestLibrary.Tests/CalculatorTests.cs
+++ TestLibrary/TestLibrary.Tests/CalculatorTests.cs
@@ -2,106 +2,75 @@
 using System.Collections.Generic;
 using NUnit.Framework;
 using NSubstitute;
+using NUnit.Core;
 
 namespace TestLibrary.Tests
 {
     [TestFixture]
     public class CalculatorTests
     {
+        private IDataReader dataReader;
+        private IRepository repository;
+        private Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            dataReader = Substitute.For<IDataReader>();
+            repository = Substitute.For<IRepository>();
+            calculator = new Calculator(dataReader, repository);
+        }
+
         [Test]
         public void CalculatorTest_NoPoints_Success()
         {
-            const int result = 0;
-            var dataReader = Substitute.For<IDataReader>();
-            var repository = Substitute.For<IRepository>();
             dataReader.ReadDataPoints("", 0).ReturnsForAnyArgs(info => new List<int>());
 
-            CalculationResult expectedResult = new CalculationResult
-            {
-                TotalAmount = result
-            };
-
-            Calculator calc = new Calculator(dataReader, repository);
-            var actualResult = calc.ProcessData(Arg.Any<string>(), 0);
+            var actualResult = calculator.ProcessData(Arg.Any<string>(), 0);
 
             repository.Received().Save(actualResult);
-
-            Assert.IsTrue(Comparer.Compare(actualResult, expectedResult));
+            Assert.AreEqual(actualResult.TotalAmount, 0);
         }
 
         [Test]
         public void CalculatorTest_SinglePoint_Success()
         {
-            const int result = 10;
-            var dataReader = Substitute.For<IDataReader>();
-            var repository = Substitute.For<IRepository>();
-            dataReader.ReadDataPoints("", 1).ReturnsForAnyArgs(info => new List<int> { result });
+            dataReader.ReadDataPoints("", 1).ReturnsForAnyArgs(info => new List<int> { 10 });
 
-            CalculationResult expectedResult = new CalculationResult
-            {
-                TotalAmount = result
-            };
-
-            Calculator calc = new Calculator(dataReader, repository);
-            var actualResult = calc.ProcessData(Arg.Any<string>(), 1);
+            var actualResult = calculator.ProcessData(Arg.Any<string>(), 1);
 
             repository.Received().Save(actualResult);
-
-            Assert.IsTrue(Comparer.Compare(actualResult, expectedResult));
+            Assert.AreEqual(actualResult.TotalAmount, 10);
         }
 
         [Test]
         public void CalculatorTest_TwoPoints_Success()
         {
-            const int result = 14;
-            var dataReader = Substitute.For<IDataReader>();
-            var repository = Substitute.For<IRepository>();
             dataReader.ReadDataPoints("", 1).ReturnsForAnyArgs(info => new List<int> { 6, 8 });
 
-            CalculationResult expectedResult = new CalculationResult
-            {
-                TotalAmount = result
-            };
-
-            Calculator calc = new Calculator(dataReader, repository);
-            var actualResult = calc.ProcessData(Arg.Any<string>(), 2);
+            var actualResult = calculator.ProcessData(Arg.Any<string>(), 2);
 
             repository.Received().Save(actualResult);
-
-            Assert.IsTrue(Comparer.Compare(actualResult, expectedResult));
+            Assert.AreEqual(actualResult.TotalAmount, 14);
         }
 
         [Test]
         public void CalculatorTest_FivePoints_Success()
         {
-            const int result = 15;
-            var dataReader = Substitute.For<IDataReader>();
-            var repository = Substitute.For<IRepository>();
             dataReader.ReadDataPoints("", 1).ReturnsForAnyArgs(info => new List<int> { 1, 2, 3, 4, 5 });
 
-            CalculationResult expectedResult = new CalculationResult
-            {
-                TotalAmount = result
-            };
-
-            Calculator calc = new Calculator(dataReader, repository);
-            var actualResult = calc.ProcessData(Arg.Any<string>(), 5);
+            var actualResult = calculator.ProcessData(Arg.Any<string>(), 5);
 
             repository.Received().Save(actualResult);
-
-            Assert.IsTrue(Comparer.Compare(actualResult, expectedResult));
+            Assert.AreEqual(actualResult.TotalAmount, 15);
         }
 
         [Test]
         public void CalculatorTest_ThrowsFormatException_Fail()
         {
-            var dataReader = Substitute.For<IDataReader>();
-            var repository = Substitute.For<IRepository>();
             dataReader.ReadDataPoints(Arg.Any<string>(), Arg.Any<int>()).Returns(info => new List<int> { 1, 2, 3, 4, 5 });
 
-            Calculator calc = new Calculator(dataReader, repository);
-
-            Assert.Throws<FormatException>(() => calc.ProcessData(Arg.Any<string>(), 3));
+            Assert.Throws<FormatException>(() => calculator.ProcessData(Arg.Any<string>(), 3));
 
             repository.DidNotReceiveWithAnyArgs().Save(null);
         }
@@ -109,13 +78,9 @@ namespace TestLibrary.Tests
         [Test]
         public void CalculatorTest_ThrowsArgumentException_Fail()
         {
-            var dataReader = Substitute.For<IDataReader>();
-            var repository = Substitute.For<IRepository>();
             dataReader.ReadDataPoints(Arg.Any<string>(), Arg.Any<int>()).Returns(info => new List<int> { -1, -2, -3, -4, 5 });
 
-            Calculator calc = new Calculator(dataReader, repository);
-
-            Assert.Throws<ArgumentException>(() => calc.ProcessData(Arg.Any<string>(), 5));
+            Assert.Throws<ArgumentException>(() => calculator.ProcessData(Arg.Any<string>(), 5));
 
             repository.DidNotReceiveWithAnyArgs().Save(null);
         }
